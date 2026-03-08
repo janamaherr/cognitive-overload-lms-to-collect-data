0a1c55a1df0891d7280e70f0903fd9b27e0473b1
janamaherr <jana.maher36@gmail.com>
2026-03-08 02:29:26 +0200

    cognitive-overload-lms-to-collect-data

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CognitiveOverloadLMS.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
