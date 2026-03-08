fce606361df42f6aea894e0919feabde93e2f9fe
janamaherr <jana.maher36@gmail.com>
2026-03-08 03:31:34 +0200

    Update behaviorLogger.js

--- wwwroot/js/behaviorLogger.js
+++ wwwroot/js/behaviorLogger.js
@@ -24,3 +24,15 @@ function logBehavior(data){
     });
 
 }
+
+// function logBehavior(data){
+
+//     fetch("/Behavior/LogBehavior", {
+//         method: "POST",
+//         headers:{
+//             "Content-Type":"application/json"
+//         },
+//         body: JSON.stringify(data)
+//     });
+
+// }
